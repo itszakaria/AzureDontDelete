8ab2e41e3b895b2aeb19e25d16990e8c897b0092
itszakaria <itszakaria@gmail.com>
2021-08-28 16:58:22 -0400

    first commit

---
+++ src/test/java/testClasses/TestAzureCloud.java
@@ -0,0 +1,27 @@
+package testClasses;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestAzureCloud {
+	
+	WebDriver dr;
+	
+	@Test
+	public void testAzureCloud() {
+		System.setProperty("webdriver.chrome.driver", "C:/lib_drivers/chromedriver.exe");
+		dr = new ChromeDriver();
+		
+		dr.get("https://dev.azure.com/itszakariabd");
+		
+		WebElement el = dr.findElement(By.xpath("//*[text()='Sign in']"));
+		if(el.isDisplayed())
+			Assert.assertTrue("test is passsed", el.isDisplayed());
+		
+	}
+
+}
